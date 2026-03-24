7984696ad196ee821e07d7ff9ab350e77a680581
vabsm10-droid <vabsm10@gmail.com>
2026-03-24 08:22:44 -0300

    Create loja_eletronica database and tables

---
+++ loja_eletronica.sql
@@ -0,0 +1,22 @@
+
+
+CREATE DATABASE loja_eletronica;
+USE loja_eletronica;
+
+CREATE TABLE fornecedores (
+    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100)
+);
+
+CREATE TABLE pecas (
+    id_peca INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10,2),
+    quantidade INT,
+    preco_fornecedor DECIMAL(10,2),
+    id_fornecedor INT,
+    CONSTRAINT fk_fornecedor
+    FOREIGN KEY (id_fornecedor)
+    REFERENCES fornecedores(id_fornecedor)
+);
